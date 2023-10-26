f9efb8d258b4f2161e9cc59f73fd2a71131bc0e6
muradmikayilzadeh <m.murad@code.edu.az>
2023-10-25 18:39:43 -0700

    Update library.py

--- library.py
+++ library.py
@@ -180,20 +180,33 @@ class CustomTukeyTransformer(BaseEstimator, TransformerMixin):
 
 class CustomRobustTransformer(BaseEstimator, TransformerMixin):
   def __init__(self, column):
-    self.column = column
+    #fill in rest below
+    self.target_column = column
+    self.iqr = None
+    self.med = None
 
-  def fit (self,df, y=None):
-    self.df = df.copy()
-    self.iqr = self.df[self.column].quantile(.75) - self.df[self.column].quantile(.25)
-    self.med = self.df[self.column].median()
+  def fit(self, X, y = None):
+    assert isinstance(X, pd.core.frame.DataFrame), f'{self.__class__.__name__}.fit expected Dataframe but got {type(X)} instead.'
+    assert self.target_column in X.columns.to_list(), f'{self.__class__.__name__}.fit unrecognizable column {self.target_column}.'
+    self.iqr = float(X[self.target_column].quantile(.75) - X[self.target_column].quantile(.25))
+    self.med = X[self.target_column].median()
     return self
 
-  def transform(self,df):
-    self.fit(df)
-    self.df[self.column] -= self.med
-    self.df[self.column] /= self.iqr
-    self.df[self.column].min(), self.df[self.column].max()
-    return self.df
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'{self.__class__.__name__}.transform expected Dataframe but got {type(X)} instead.'
+    assert isinstance(self.iqr, float), f'NotFittedError: This {self.__class__.__name__} instance is not fitted yet. Call "fit" with appropriate arguments before using this estimator.'
+    assert self.target_column in X.columns.to_list(), f'{self.__class__.__name__}.transform unrecognizable column {self.target_column}.'
+
+    X_ = X.copy()
+    X_[self.target_column] -= self.med
+    X_[self.target_column] /= self.iqr
+    return X_
+
+  def fit_transform(self, X, y = None):
+    self.fit(X,y)
+    result = self.transform(X)
+    return result
+
 
 def find_random_state(features_df, labels, n=200):
   model = KNeighborsClassifier(n_neighbors=5)  #instantiate with k=5.
